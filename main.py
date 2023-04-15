bd44a2d8382920e2f2ee62921261c6eead08897c
AlphaRomeo13 <rafifpradana31@gmail.com>
2023-04-15 21:24:08 +0900

    # '

--- main.py
+++ main.py
@@ -3,27 +3,28 @@ import phonenumbers
 from phone_number import number
 from phonenumbers import geocoder
 
-Key = '874152d605e945e7984d6d342d218679'
+Key = "874152d605e945e7984d6d342d218679"
 
 lamNumber = phonenumbers.parse(number)
-yourLocation = geocoder.description_for_number(lamNumber, 'en')
+yourLocation = geocoder.description_for_number(lamNumber, "en")
 print(yourLocation)
 
 from phonenumbers import carrier
 
 service_provider = phonenumbers.parse(number)
-print(carrier.name_for_number(service_provider, 'en'))
+print(carrier.name_for_number(service_provider, "en"))
 
 from opencage.geocoder import OpenCageGeocode
+
 geocoder = OpenCageGeocode(Key)
 query = str(yourLocation)
 results = geocoder.geocode(query)
 
-lat = results[0]['geometry']['lat']
+lat = results[0]["geometry"]["lat"]
 
-lng = results[0]['geometry']['lng']
+lng = results[0]["geometry"]["lng"]
 
-country = results[0]['annotations']['timezone']['name']
+country = results[0]["annotations"]["timezone"]["name"]
 
 print(lat, lng, country)
 
